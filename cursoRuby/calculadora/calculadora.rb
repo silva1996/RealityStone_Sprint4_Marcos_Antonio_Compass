d27d2e488583b23ced408d0f64d1bb9471920120
silva1996 <marcosj.antonio3@gmail.com>
2022-05-13 17:25:20 -0300

    adição da calculadora

--- cursoRuby/calculadora/calculadora.rb
+++ cursoRuby/calculadora/calculadora.rb
@@ -1,34 +1,93 @@
+again = 1
+while again == 1 do
+
 class Calculadora
-    def Calculadora.sumar(n,m)
-     n+m
-    end
-    def Calculadora.restar(n,m)
-     n-m 
-    end
-    def Calculadora.multiplicar(n,m)
-     n*m 
-    end
-    def Calculadora.dividir(n,m)
-     n/m 
-    end
-    end
-    
-    print "Primer valor:"
-    val1 = gets.chomp
-    
-    print "Segundo valor:"
-    val2 = gets.chomp
-    
-    print "operador (+,-,*,/):"
-    operador = gets.chomp
-    
-    puts case operador
-    when '+': Calculadora.sumar(val1.to_i,val2.to_i)
-    when '-': Calculadora.restar(val1.to_i,val2.to_i)
-    when '*': Calculadora.multiplicar(val1.to_i,val2.to_i)
-    when '/': Calculadora.dividir(val1.to_i,val2.to_i)
-    end
-    
-    #mejora > metodo.to_sym => :metodo
-    resultado = val1.to_i.send(operador.to_sym,last.to_i)
-    print "Y el resultado es: #{resultado}"
+
+	def adicao (n1, n2)
+		@res =	(n1 + n2) 
+		if @res = -
+			@res * -1
+			puts "#{n1} + #{n2} = #{@res}"
+			return @res
+		end
+	end
+
+	def subtracao(n1, n2)
+		@res =	(n1 - n2) 
+		if @res = -
+			@res * -1
+			puts "#{n1} - #{n2} = #{@res}"
+			return @res
+		end 		
+	end
+	
+
+	def divisao(n1, n2)
+		if n2 != 0
+			@res =	(n1 / n2)
+			if @res = -
+				@res * -1
+				puts "#{n1} / #{n2} = #{@res.round(2)}"
+				return @res.round(3)
+			end
+		else
+			puts "Divisao Invalida!"
+			return "Divisao Invalida!"
+		end
+	end
+
+	def multiplicacao(n1, n2)
+		@res =	(n1 * n2) 
+		if @res = -
+			@res * -1
+			puts "#{n1} * #{n2} = #{@res}"
+			return @res
+		end
+	end
+
+	def null
+		puts "Operacao Invalida!"
+	end
+end
+
+
+
+puts "Calculadora"
+puts "-----------"
+
+puts "Escolha a operacao!"
+puts "1 Adicao"
+puts "2 Subtracao"
+puts "3 Divisao"
+puts "4 Multiplicacao"
+
+puts "Digite o primeiro numero?"
+n1 = gets.chomp().to_f
+
+puts "Digite o numero da Operacao escolhida?"
+operacao = gets.chomp().to_i
+
+puts "Digite o segundo numero?"
+n2 = gets.chomp().to_f
+
+calculo = Calculadora.new
+
+if operacao == 1
+	calculo.adicao(n1, n2)
+elsif operacao == 2
+	calculo.subtracao(n1, n2)
+elsif operacao == 3
+	calculo.divisao(n1, n2)
+elsif operacao == 4
+	calculo.multiplicacao(n1, n2)
+else operacao == ""
+	calculo.null
+end
+
+again = 0
+puts ("Deseja realizar uma nova conta?")
+puts ("1 - sim")
+puts ("2 - nao")
+again = gets.to_i
+system('cls')
+end
